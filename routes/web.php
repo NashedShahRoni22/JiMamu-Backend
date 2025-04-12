effd18064a44eafbdc97cab605335f52be085b51
akazad1235 <akazad914@gmail.com>
2025-04-12 10:00:36 +0600

    db migration commnad added in web route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
+Route::get('/admin', function () {
 
     return Inertia::render('welcome');
 })->name('home');
@@ -12,6 +12,14 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+});
+Route::get('/migrate', function () {
+    try {
+        \Illuminate\Support\Facades\Artisan::call('migrate --force');  // Run migrations without removing existing data
+        return 'Migrations run successfully!';
+    } catch (\Exception $e) {
+        return $e->getMessage();
+    }
 });
 
 require __DIR__.'/settings.php';

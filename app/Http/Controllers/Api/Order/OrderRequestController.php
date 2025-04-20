a1928bf5a913c1c3fbf2bb24be45d5c720921cf9
akazad1235 <akazad914@gmail.com>
2025-04-21 00:59:42 +0600

    order request and notifications testing

--- app/Http/Controllers/Api/Order/OrderRequestController.php
+++ app/Http/Controllers/Api/Order/OrderRequestController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Models\OrderRequest;
 use App\Services\Rider\LocationService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Redis;
 
 class OrderRequestController extends Controller
 {
@@ -13,7 +15,47 @@ class OrderRequestController extends Controller
 
     }
     public function orderRequest(Request $request){
+//        $rider = auth()->id();
+//       return Redis::geopos('rider_locations', "rider:$rider");
+//        $lat = 23.7611;
+//        $lng = 90.3623;
+//
+//        $response = Http::withHeaders([
+//            'User-Agent' => 'YourAppName/1.0 (your@email.com)'
+//        ])->get("https://nominatim.openstreetmap.org/reverse", [
+//            'format' => 'json',
+//            'lat' => $lat,
+//            'lon' => $lng,
+//            'zoom' => 18,
+//            'addressdetails' => 1,
+//        ]);
+//
+//        $data = $response->json();
+//       return $address = $data['display_name'] ?? null;
 
+        // Step 1: Get all members from the geo set
+        $members = Redis::zrange('rider_locations', 0, -1);
+
+        $locations = [];
+
+        // Step 2: Loop through and get position of each rider
+        foreach ($members as $member) {
+            $position = Redis::geopos('rider_locations', $member);
+
+            if (!empty($position[0])) {
+                $locations[] = [
+                    'rider_id' => str_replace('rider:', '', $member),
+                    'longitude' => $position[0][0],
+                    'latitude' => $position[0][1],
+                ];
+            }
+        }
+        $nearby = Redis::georadius('rider_locations', 90.3582, 23.7564, 1, 'km');
+        $riderIds = collect($nearby)->map(function ($key) {
+            return str_replace('rider:', '', $key);
+        });
+
+        return $riderIds;
         try {
            $orderRequest = OrderRequest::create([
                 'user_id' => auth()->id(),
@@ -26,7 +68,15 @@ class OrderRequestController extends Controller
                 'price' => $request->price,
                 'pickup_radius' => $request->pickup_radius
             ]);
-            $this->locationService->findNearbyRiders($orderRequest);
+            $nearbyRiders = Redis::command('georadius', [
+                'rider_locations',
+                $request->pickup_longitude,
+                $request->pickup_latitude,
+                $request->pickup_radius,
+                'km',
+            ]);
+            return $nearbyRiders;
+           return $this->locationService->findNearbyRiders($orderRequest);
             return sendResponse(success: true, message: 'Successfully send order request');
         }catch (\Exception $exception){
             return sendResponse(success: false, message: 'something went wrong', data: null, status: 422,  error: $exception->getMessage());
--- app/Http/Controllers/Api/Rider/RiderLocationController.php
+++ app/Http/Controllers/Api/Rider/RiderLocationController.php
@@ -9,9 +9,12 @@ use Illuminate\Support\Facades\Redis;
 class RiderLocationController extends Controller
 {
     public function locationUpdate(Request $request){
+        Redis::geoadd('rider_locations', 90.36, 23.76, 'rider:6');
+        Redis::geoadd('rider_locations', 90.35, 23.755, 'rider:7');
         try {
-            $id =  auth()->id();
-            Redis::geoadd('rider_locations', $request->latitude, $request->longitude, "rider:$id");
+           // $id =  auth()->id();
+//            $id =  7;
+//            Redis::geoadd('rider_locations', $request->latitude, $request->longitude, "rider:$id");
             return sendResponse(success: true, message: 'successfully updated locations.');
         }catch (\Exception $exception){
             return sendResponse(success: false, message: 'something went wrong.');

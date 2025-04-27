1f1e5371bbb6ddf6abf0a95b1399fc9319524ec9
akazad1235 <akazad914@gmail.com>
2025-04-27 21:02:08 +0600

    profile show response udpate

--- app/Http/Resources/ProfileUpdateResource.php
+++ app/Http/Resources/ProfileUpdateResource.php
@@ -20,7 +20,8 @@ class ProfileUpdateResource extends JsonResource
             'email' => $this->email,
             'phone_number' => $this?->phone_number,
             'profile_image' => $this?->profile_image,
-            'dod' => $this->dod,
+            'dob' => $this->dod,
+            'gender' => $this->gender,
             'status' => array_search($this->status, User::$status) ?? 'unknown', // Convert integer to string
             'role' => $this->getRoleNames()->toArray(),
             'rider_bank_information' => RiderBankInformationResource::collection($this->riderBankInformations),

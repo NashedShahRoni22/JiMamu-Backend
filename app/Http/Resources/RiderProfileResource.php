9c3645f8003999114bf5c1187d3d70d6cbf4dcbb
akazad1235 <akazad914@gmail.com>
2025-09-26 16:20:49 +0600

    rider id added on rider profile

--- app/Http/Resources/RiderProfileResource.php
+++ app/Http/Resources/RiderProfileResource.php
@@ -16,6 +16,7 @@ class RiderProfileResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'user_id' => $this->id,
             'status' => array_search($this->status, User::$status) ?? 'unknown', // Convert integer to string
             'role' => $this->getRoleNames()->toArray(),
             'rider_document' => RiderDocumentResource::collection($this->userRiders),

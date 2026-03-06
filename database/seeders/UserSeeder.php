d288ce479735fc5a96498d66260840089b76f740
akazad1235 <akazad914@gmail.com>
2026-03-06 21:58:28 +0600

    disable redis for user geoloaction from user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -115,17 +115,17 @@ class UserSeeder extends Seeder
         $rider10->assignRole($riderRole);
         $user->assignRole($userRole);
 
-        Redis::geoadd('rider_locations', 23.7615, 90.3671, "rider:{$rider->id}");
-        Redis::geoadd('rider_locations', 23.7597, 90.3746, "rider:{$rider1->id}");
-        Redis::geoadd('rider_locations', 23.7688, 90.3653, "rider:{$rider2->id}");
-        Redis::geoadd('rider_locations', 23.7602, 90.3630, "rider:{$rider3->id}");
-        Redis::geoadd('rider_locations', 23.7544, 90.3662, "rider:{$rider4->id}");
-        Redis::geoadd('rider_locations', 23.7515, 90.3685, "rider:{$rider5->id}");
-        Redis::geoadd('rider_locations', 23.7469, 90.3552, "rider:{$rider6->id}");
-        Redis::geoadd('rider_locations', 23.7512, 90.3607, "rider:{$rider7->id}");
-        Redis::geoadd('rider_locations', 23.7566, 90.3741, "rider:{$rider8->id}");
-        Redis::geoadd('rider_locations', 23.7460, 23.7460, "rider:{$rider9->id}");
-        Redis::geoadd('rider_locations', 23.7460, 23.7460, "rider:{$rider10->id}");
+//        Redis::geoadd('rider_locations', 23.7615, 90.3671, "rider:{$rider->id}");
+//        Redis::geoadd('rider_locations', 23.7597, 90.3746, "rider:{$rider1->id}");
+//        Redis::geoadd('rider_locations', 23.7688, 90.3653, "rider:{$rider2->id}");
+//        Redis::geoadd('rider_locations', 23.7602, 90.3630, "rider:{$rider3->id}");
+//        Redis::geoadd('rider_locations', 23.7544, 90.3662, "rider:{$rider4->id}");
+//        Redis::geoadd('rider_locations', 23.7515, 90.3685, "rider:{$rider5->id}");
+//        Redis::geoadd('rider_locations', 23.7469, 90.3552, "rider:{$rider6->id}");
+//        Redis::geoadd('rider_locations', 23.7512, 90.3607, "rider:{$rider7->id}");
+//        Redis::geoadd('rider_locations', 23.7566, 90.3741, "rider:{$rider8->id}");
+//        Redis::geoadd('rider_locations', 23.7460, 23.7460, "rider:{$rider9->id}");
+//        Redis::geoadd('rider_locations', 23.7460, 23.7460, "rider:{$rider10->id}");
 
         // create pacakges
         Package::create([
